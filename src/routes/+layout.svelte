57204b4eb5c5827b3bb87f6ff20dffb3cdde360b
Ryank2004 <ryankorver04@gmail.com>
2024-09-24 16:03:16 +0200

    Verplaats wijzigingen naar nieuwe branch

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/webinars">Webinars</a>
+        </li>
+        <li>
+            <a href="/contouring">Contouring</a>
+        </li>
+        <li>
+            <a href="/profile">Profile</a>
+        </li>
+    </ul>
+</nav>
+
+<slot />
+
+<style>
+    nav {
+        display: flex;
+        margin-top: auto;
+    }
+</style>
