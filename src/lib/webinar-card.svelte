c9a3c2a354522f65b815dd7738c6065df9841f84
zoepje <zoe.penner@hva.nl>
2024-09-25 16:06:45 +0200

    #13 begining card html

---
+++ src/lib/webinar-card.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let data
+</script>
+
+<article>
+  <a href="/{data.webinar.slug}">
+    <div class="container-image">
+      <img src="{data.webinar.image}" alt="{data.webinar.category}">
+      <p>{data.webinar.duration}</p>
+    </div>
+    <h3>{data.webinar.title}</h3>
+    <p>{data.webinar.speaker}</p>
+    <div class="category-watch">
+      <p class="category">{data.webinar.category}</p>
+      <p class="watch-link">Watch</p>
+    </div>
+  </a>
+</article>
