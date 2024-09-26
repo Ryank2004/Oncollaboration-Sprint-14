1c0e1ec3e9190153e03928e2475b8b349f35f9a5
zoepje <zoe.penner@hva.nl>
2024-09-26 21:09:39 +0200

    styling card #13

--- src/lib/webinar-card.svelte
+++ src/lib/webinar-card.svelte
@@ -1,18 +1,104 @@
 <script>
-  export let data
+  export let width = '270px';
 </script>
 
-<article>
-  <a href="/{data.webinar.slug}">
+<article style="--width: {width};">
+  <a href="/">
     <div class="container-image">
-      <img src="{data.webinar.image}" alt="{data.webinar.category}">
-      <p>{data.webinar.duration}</p>
+      <img src="https://fdnd-agency.directus.app/assets/ed2c1154-3eac-4b96-819d-64d7fb2ad1a9" alt="thumbnail">
+      <p class="duration">1:43:39</p>
     </div>
-    <h3>{data.webinar.title}</h3>
-    <p>{data.webinar.speaker}</p>
-    <div class="category-watch">
-      <p class="category">{data.webinar.category}</p>
-      <p class="watch-link">Watch</p>
+    <h3>IROS CME Webinar: Fractionation in Head & Neck and CNS malignancies; Radiobiology Perspective</h3>
+    <p class="speaker">Prof. R.P. (Rob) Coppes, PhD</p>
+    <div class="categories">
+      <p class="category">Head & Neck</p>
     </div>
   </a>
 </article>
+
+<style>
+  * {
+    margin: 0;
+  }
+
+  article {
+    margin-right: 1rem;
+    max-width: var(--width);
+    
+    & a {
+      color: black;
+      text-decoration: none;
+    }
+
+    & p {
+      font-size: 9px;
+    }
+
+    & .container-image{
+      position: relative;
+
+      & img {
+        width: 100%;
+        height: 80%;
+        object-fit: contain;
+        border-radius: 10px;
+      }
+
+      & .duration {
+        position: absolute;
+        background-color: #1F000B;
+        color: white;
+        margin: 0;
+        padding: 4px;
+        bottom: 10px;
+        right: 10px;
+        border-radius: 5px;
+        z-index: 1;
+      }
+    }
+
+    & h3 {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      word-break: break-all;
+      font-size: 13px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    & .speaker {
+      text-transform: uppercase;
+      color: #6A6A6A;
+      margin-block: .2rem;
+    }
+
+    & .category {
+      display: none;
+      padding: 4px;
+      width: fit-content;
+      background-color: #6A002580;
+      border-radius: 5px;
+    }
+  }
+
+  @media only screen and (min-width: 600px){
+    article {
+      margin-right: 2rem;
+      max-width: 384px;
+    }
+
+    article p {
+      font-size: 13px;
+    }
+
+    article h3 {
+      -webkit-line-clamp: 2;
+      font-size: 20px;
+    }
+
+    article .category {
+      display: block;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+  import WebinarCard from "../lib/webinar-card.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <WebinarCard />
+
+  <WebinarCard />
+</main>
+
+<style>
+  main{
+    display: flex;
+  }
+</style>
