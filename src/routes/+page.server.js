3bbfca2b85484a4ba55b5cb8453411a657b75d88
zoepje <zoe.penner@hva.nl>
2024-09-27 18:28:14 +0200

    Opladen van verschillende kaartjes #25

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,9 @@
 import fetchJson from "../lib/fetch-json";
 
 export async function load() {
-  const url = 'https://fdnd-agency.directus.app/items/dda_agencies/1'
+  const url = 'https://fdnd-agency.directus.app/items/avl_webinars'
   const webinars = await fetchJson(url)
-
+  
   return {
     webinars: webinars.data
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,18 @@
   export let data;
 </script>
 
-<h1>Welcome to SvelteKit "Fractionation"</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <main>
+  {#each data.webinars as webinar}
   <WebinarCard 
-  title = {data.webinars.title}
-  speakers = {data.webinars.vacancies}
-  categories = {data.webinars.vacancies} />
-
-  <WebinarCard 
-  title = "26th FARO Webinar: Updated management in glioma WHO grade 2-3"
-  speakers = ""
-  categories = "Gastrointestinal"/>
+    slug = {webinar.slug}
+    thumbnail = {webinar.thumbnail}
+    duration = {webinar.duration}
+    title = {webinar.title}
+    speakers = {webinar.speakers}
+    categories = {webinar.categories} />
+  {/each}
 </main>
+
 <style>
   main{
     display: flex;
