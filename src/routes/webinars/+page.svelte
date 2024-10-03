cbcac97db5f8e9df49110ff5ba2c6d1575e35b4d
zoepje <zoe.penner@hva.nl>
2024-10-03 15:17:20 +0200

    Maak de fetch api voor elke pagina

--- src/routes/webinars/+page.svelte
+++ src/routes/webinars/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import Search from "$lib/search.svelte"
   import Filter from "$lib/filter.svelte";
+  import WebinarCard from "$lib/webinar-card.svelte";
 
+  export let data
 </script>
 
 <main>
@@ -10,10 +12,26 @@
   <Search />
 
   <Filter />
+
+  <section>
+    {#each data.webinars as webinar}
+    <WebinarCard 
+      slug = {webinar.slug}
+      thumbnail = {webinar.thumbnail}
+      duration = {webinar.duration}
+      title = {webinar.title}
+      speakers = {webinar.speakers}
+      categories = {webinar.categories} />
+    {/each}
+  </section>
 </main>
 
 <style>
   main {
     width: 100vw;
+  }
+
+  section {
+    display: flex;
   }
 </style>
