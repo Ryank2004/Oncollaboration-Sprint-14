45de25589653e5140a8ead057339a914ad596e37
zoepje <zoe.penner@hva.nl>
2024-10-08 11:21:12 +0200

    Delete index.js

--- src/lib/index.js
+++
@@ -1,6 +0,0 @@
-import ContouringOverview from "./CountouringOverview.svelte";
-import Search from "./Search.svelte";
-import Filter from "./Filter.svelte";
-import WebinarOverview from "./WebinarOverview.svelte";
-
-export {ContouringOverview, Search, Filter, WebinarOverview}
