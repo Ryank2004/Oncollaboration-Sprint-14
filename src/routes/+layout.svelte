dac2724c9a22923b670ce9111d60352ebcbb5bf5
Ryank2004 <ryankorver04@gmail.com>
2024-10-09 14:13:50 +0200

    vercel fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,12 +13,18 @@
 @media (min-width: 1080px) {
   .slot {
     margin-top: 64px;
-      }
+    }
 }
+
+/* @media (min-width: 1080px) {
+  .slot {
+    margin-top: 64px;
+    }
+} */
 
 @media (min-width: 1920px) {
   .slot {
     margin-top: 72px;
-      }
+    }
 }
 </style>
